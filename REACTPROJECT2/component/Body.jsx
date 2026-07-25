04ccfbc272a03f30ea3953d183a93ed59e120e99
SAHILMISHRA-9 <sahilmishra9922@gmail.com>
2026-07-26 04:37:37 +0530

    Building react dynamic project

--- REACTPROJECT2/component/Body.jsx
+++ REACTPROJECT2/component/Body.jsx
@@ -1,14 +1,36 @@
 export function Body(){
-    function handleClick(){
-        console.log('I was clicked')
+    
+    const ingredient=["chicken","tomato","organo"]
+
+    const ingredientList=ingredient.map(ingredient =>(
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+    function handleSubmit(event){
+        event.preventDefault()
+        // console.log('I was clicked')
+        const Formdata= new FormData(event.currentTarget)
+        const newingredient=Formdata.get("ingredient")
+        // console.log(newingredient)
+        ingredient.push(newingredient)
+        console.log(ingredient)
     }
+
     return(
         <main>
-            <form className="form">
-                <input aria-label="Add ingredient" type="text" placeholder="e.g. organo"></input>
-                <button onClick={handleClick} type="button">+ Add ingredient</button>
+            <form className="form" onSubmit={handleSubmit}>
+                <input 
+                    aria-label="Add ingredient" 
+                    type="text" 
+                    placeholder="e.g. organo"
+                    name="ingredient"
+                    >
+                </input>
+                <button  type="submit">+ Add ingredient</button>
             </form>
-            
+            <ul>
+                {ingredientList}
+            </ul>
         </main>
     )
 }
