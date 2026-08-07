dde10b28fbfa34264b9e9e2af8988017cfdea319
SAHILMISHRA-9 <sahilmishra9922@gmail.com>
2026-08-07 17:41:26 +0530

    new files

--- REACTCHALL8/vite-project/component/WindowTracker.jsx
+++ REACTCHALL8/vite-project/component/WindowTracker.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 
 export default function WindowTracker(){
+    const [windowWidth, setwindowWidth]=React.useState(window.innerWidth)
     // Since we want to change the outer 
     // property from the inner we can use UseEffect
+    React.useEffect(()=>{
+        function watchWindowWidth(){
+            setwindowWidth(window.innerWidth)
+        }
+        window.addEventListener("resize",watchWindowWidth)
+        return function() {
+            window.removeEventListener("resize",watchWindowWidth)
+        }
+    },[])
 
     return(
-        <h1>Window width: {window.innerWidth}</h1>
+        <h1>Window width: {windowWidth}</h1>
         
     )
 }
