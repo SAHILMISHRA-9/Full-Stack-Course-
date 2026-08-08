4b3c4f3ffb7ac3fdf9d87525f6274dc949858bd2
SAHILMISHRA-9 <sahilmishra9922@gmail.com>
2026-08-08 23:50:03 +0530

    new files

--- EndGame/vite-project/src/App.jsx
+++ EndGame/vite-project/src/App.jsx
@@ -1,32 +1,45 @@
-// import { useState,useEffect,useRef } from 'react'
+import { useState,useEffect,useRef } from 'react'
+import { languages } from '../assets/language'
 import './App.css'
 
 function App() {
+
+  const [currentWord,setCurrentWord]=useState("react")
+  
+  const letterElements=
+
+
   return (
     <main>
+
       <header className='header'>
         <h1>Assembly: EndGame</h1>
         <p>Guess the word within 8 attemot to 
           keep the program word safe from Assembly</p>
       </header>
+
       <section className='result'>
         <h2>Game Over!</h2>
         <p>You lose! Better start learning Assembly</p>
       </section>
+
       <section className='Techstack'>
-        <button>HTMl</button>
-        <button>CSS</button>
-        <button>Javascript</button>
-        <button>React</button>
-        <button>Typescript</button><br />
-        <button>Node.js</button>
-        <button>Python</button>
-        <button>Ruby</button>
-        <button>Assembly</button>
+        {languages.map((label)=>(
+          <button 
+          style={{
+            backgroundColor: label.backgroundColor,
+            color: label.color,
+          }}
+          >
+            {label.name}
+          </button>
+        ))}
       </section>
-      <section>
-        <input type='character'/>
-      </section>
+
+      <span>
+
+      </span>
+  
     </main>
   )
 }
